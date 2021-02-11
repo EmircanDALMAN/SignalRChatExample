9f4efe09e99f1c278e1b55d916a9ebf19e14b6b5
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-02-11 15:34:58 +0300

    v1.1

---
+++ SignalRChatExample/SignalRChatExample.ServerUI/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChatExample.ServerUI.Models
+{
+    public class Client
+    {
+        public string ConnectionId { get; set; }
+        public string NickName { get; set; }
+    }
+}
