0ae5006b60a95bb5ead5e5fbd82be40b934c346a
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-02-22 16:41:45 +0300

    v1.1

---
+++ SignalRChatExample/SignalRChatExample.ServerUI/Data/ClientSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SignalRChatExample.ServerUI.Models;
+
+namespace SignalRChatExample.ServerUI.Data
+{
+    public static class ClientSource
+    {
+        public static List<Client> Clients { get; } = new List<Client>();
+    }
+}
--- SignalRChatExample/SignalRChatExample.ServerUI/Hubs/ChatHub.cs
+++ SignalRChatExample/SignalRChatExample.ServerUI/Hubs/ChatHub.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using SignalRChatExample.ServerUI.Data;
+using SignalRChatExample.ServerUI.Models;
 
 namespace SignalRChatExample.ServerUI.Hubs
 {
     public class ChatHub : Hub
     {
-
+        public async Task GetNickName(string nickName)
+        {
+            Client client = new Client
+            {
+                ConnectionId = Context.ConnectionId,
+                NickName = nickName
+            };
+            ClientSource.Clients.Add(client);
+            await Clients.Others.SendAsync("clientJoined",nickName);
+            await Clients.All.SendAsync("clients", ClientSource.Clients);
+        }
     }
 }
